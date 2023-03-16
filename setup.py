bfb41d7aafee5f4254dd1c8ddcf1586f39eed5c7
Ifeanyi <ifeanyi@anyi.local>
2023-03-16 16:46:18 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-# coding: utf-8
+#!/usr/bin/env python
 ##https://gist.github.com/juancarlospaco/75d258d6ffb6e15f395c
 from setuptools import find_packages, setup
-from json import load as json_load
+import json 
+
 
 if __name__ == '__main__':
-    with open('setup.json', 'r') as info:
-        setup_kwargs = json_load(info)
+    with open('setup.json','r') as info:
+        kwargs = json.load(info)
     setup(
-        packages=find_packages(exclude=['docs', 'tests','examples']),
-        **setup_kwargs
+        packages=find_packages(),
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        **kwargs
     )
 
